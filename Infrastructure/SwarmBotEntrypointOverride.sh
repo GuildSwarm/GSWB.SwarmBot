f8162ed26e424ffb5d0e8733114c95ac22b4e75f
retributter <alejandrobinarycode@gmail.com>
2024-06-09 12:18:09 +0200

    GSWI-95: Adapting swarmbot

--- Infrastructure/SwarmBotEntrypointOverride.sh
+++ Infrastructure/SwarmBotEntrypointOverride.sh
@@ -9,23 +9,6 @@ set -e
 # "import" service await functions
 source wait_for_service.sh
 
-# Execute tasks before the base entrypoint starts
-execute_before_start() {
-    echo "Executing scheduled tasks before the base entrypoint starts..."	
-    wait_IsReady vault.vault.svc.cluster.local 9200 # Vault waits until consul is ready :)
-    echo "Scheduled tasks before the base entrypoint starts..DONE."
-}
-
-
-
-# Execute tasks after the base entrypoint started.
-execute_after_start() {
-    echo "Executing scheduled tasks after the base entrypoint started..."	
-    wait_for_health_endpoint
-    ./IsReadyServer.sh &	
-    echo "Scheduled tasks after the base entrypoint started..DONE."
-}
-
 # Main function that orchestrates the execution of the script, executing first the function with the custom logic that should be executed before calling the base entrypoint. 
 # Secondly calls the base entrypoint and finally executed the function with the custom logic that should be executed after the base entripoint is called.
 main() {
